cc7cada4480d33e1b645a4b00581503ad975ebf6
Adam Lysén <adam.lysen@elev.ga.ntig.se>
2024-11-26 10:19:03 +0100

    gdfgdfgdr

---
+++ Del02/Program.cs
@@ -0,0 +1,7 @@
+﻿List<string> leksaker = ["Bil", "Lego", "Pussel", "Cykel", "Boll"];
+List<string> Namn = ["Rasmus", "Simon", "Wiggo", "Elias", "Bullen"];
+List<int> nummer= [1, 4, 8, 5, 2];
+
+
+Console.ReadLine();
+
